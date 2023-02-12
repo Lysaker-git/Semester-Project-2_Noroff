0655854f1de356a67a0b6b3a8530710c1b991de8
Lysaker-git <68820814+Lysaker-git@users.noreply.github.com>
2023-02-12 18:48:53 +0100

    forms for login genereated with js

--- src/js/templates/forms.js
+++ src/js/templates/forms.js
@@ -1,39 +1,32 @@
-
-
-export function creatingForms (elements) {
-    console.log("form hello");
-    const signUpContainer = document.querySelector('.signUp');
-    const signInContainer = document.querySelector('signIn');
-
+function addElementsForSignIn(fullForm, elements) {
     elements.forEach(element => {
-        let signingIn = element["sign-in"];
-        if (signingIn) {
+        let title = element["title"];
+        if (element["sign-in"]) {
             let label = document.createElement("label");
             let input = document.createElement("input");
-            label.setAttribute("for", element["title"]);
-            input.setAttribute("type", element["title"]);
-            input.setAttribute("name", element["title"]);
-            console.log(label)
-            console.log(input)
 
-
+            label.setAttribute("for", title);
+            label.innerText = `${title}:`;
+            
+            input.setAttribute("type", title);
+            input.setAttribute("name", title);
+            input.id = title;
+            
+            fullForm.append(label, input);
         }
     })
-    // Creating sign up form
+}
 
-    // signUpContainer.innerHTML = `
-    //     <label for "name">Name:</label>
-    //     <input type="text" name="name" id="name">
-    //     <label for="avatar">Avatar:</label>
-    //     <input type="text" name="avatar" id="avatar">
-    //     <label for="email">E-mail:</label>
-    //     <input type="email" name="email" id="email">
-    //     <label for="confirmEmail">Confirm E-mail:</label>
-    //     <input type="email" name="confirmEmail" id="confirmEmail"> 
-    //     <label for="passwordSignUp">Password:</label>
-    //     <input type="password" name="passwordSignUp" id="passwordSignUp">
-    //     <label for="passwordSignUpConfirm">Password:</label>
-    //     <input type="password" name="passwordSignUpConfirm" id="passwordSignUpConfirm">
-    //     <button type="submit">Sign Up</button>
-    // `
+export function creatingForms (elements) {
+    const signUpContainer = document.querySelector('.signUp');
+    const signInContainer = document.querySelector('.signIn');
+    const fullForm = document.createElement('div');
+    const button = document.createElement("button");
+    
+    addElementsForSignIn(fullForm, elements);
+    
+    let formLength = fullForm.children.length;
+    button.innerText = formLength >= 4 ? "Sign In" : "Sign Up";
+
+    signInContainer.append(fullForm, button);
 }
